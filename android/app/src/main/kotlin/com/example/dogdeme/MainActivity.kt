ce945046f21acfe7e7c0b3fc44eda5cc137ba4cc
Darklord1234938 <117258557+Darklord1234938@users.noreply.github.com>
2025-02-20 11:42:53 +0100

    d

--- android/app/src/main/kotlin/com/example/dogdeme/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.dogdeme
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dogdeme/Dogde.dart';
-import 'package:flame/game.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  Dogde game = Dogde();
-  runApp(GameWidget(game: game));
-}
-
